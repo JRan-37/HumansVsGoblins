8e602dbf728c85374c772afd3d8edd2a2b764154
JRan-37 <79283020+JRan-37@users.noreply.github.com>
2022-05-02 13:14:31 -0400

    Comments and refactoring

--- src/java/Listeners/EventListenerBase.java
+++ src/java/Listeners/EventListenerBase.java
@@ -2,4 +2,5 @@ package Listeners;
 
 import java.util.EventListener;
 
-public interface EventListenerBase extends EventListener { };
+//Parent class of EventListener without generic class implementation
+public interface EventListenerBase extends EventListener { }
