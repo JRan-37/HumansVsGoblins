ab7f04dde6d38c6a3fdf3bcd8c215ec844e597bd
JRan-37 <79283020+JRan-37@users.noreply.github.com>
2022-05-02 12:59:38 -0400

    Comments and refactoring

--- src/java/Environment/Grid.java
+++ src/java/Environment/Grid.java
@@ -4,55 +4,59 @@ import Utils.Position;
 
 import java.util.Arrays;
 
+//Handles grid generation and display
 public class Grid {
 
-    private GridTiles[][] map;
-    private int tileSize;
-    private int sizeX;
-    private int sizeY;
+    private GridTiles[][] map; //Holds a grid of tile objects
+    private int tileSize; //Size of individual tiles, odd numbers recommended
+    private int sizeX; //size of grid along X axis
+    private int sizeY; //size of grid along Y axis
 
+    //Initializes grid and fills with grass tiles TODO implement other tiles
     public Grid(int sizeX, int sizeY, int tileSize) {
         this.tileSize = tileSize;
         this.sizeX = sizeX;
         this.sizeY = sizeY;
-        map = new GridTiles[sizeY][sizeX];
-        //Arrays.stream(map).forEach(x -> Arrays.fill(x, new GridTiles(tileSize, Tiles.GRASS)));
+        map = new GridTiles[sizeY][sizeX]; //initialize grid
         for(int i = 0; i < sizeY; i++) {
             for(int j = 0; j < sizeX; j++) {
-                map[i][j] = new GridTiles(tileSize, Tiles.GRASS, new Position(i, j));
+                map[i][j] = new GridTiles(tileSize, Tiles.GRASS, new Position(i, j)); //fill grid with tiles
             }
         }
     }
 
+    //returns a tile at given position, returns null if position is invalid
     public GridTiles getTileAtPosition(Position pos) {
         try {
-            map[pos.posY()][pos.posX()].setColors(false);
             return map[pos.posY()][pos.posX()];
         }
         catch(IndexOutOfBoundsException e) {
-            System.out.println("No Tile");
             return null;
         }
     }
 
+    //displays grid to the terminal
     public void displayMap() {
-        int displaySize = map.length * tileSize;
-        String[] mapString = new String[displaySize];
+        int displaySize = map.length * tileSize; //size required to display each character
+        String[] mapString = new String[displaySize]; //string array holds horizontal lines of tile data in each element
         Arrays.fill(mapString, "");
 
+        //Iterate through each grid tile
         for(int i = 0; i < sizeY; i++) {
             for(int j = 0; j < sizeX; j++) {
-                String[][] currentTile = map[i][j].toStringArray();
+                String[][] currentTile = map[i][j].toStringArray(); //load tile data of the grid tile
+                //Iterate through each tile element
                 for(int k = 0; k < tileSize; k++) {
                     for(int l = 0; l < tileSize; l++){
-                        mapString[i*tileSize + k] += currentTile[k][l];
+                        mapString[i*tileSize + k] += currentTile[k][l]; //add tile string representation to display string
                     }
                 }
             }
         }
 
-        for(int i = 0; i < mapString.length; i++) {
-            System.out.println(mapString[i]);
+        //Iterate through each line of the display string
+        for (String s : mapString) {
+            System.out.println(s); //display line
         }
     }
 }
--- src/java/Environment/Tiles.java
+++ src/java/Environment/Tiles.java
@@ -1,5 +1,6 @@
 package Environment;
 
+//Enum defining possible types of tiles
 public enum Tiles {
     GRASS,
     ROCK,
