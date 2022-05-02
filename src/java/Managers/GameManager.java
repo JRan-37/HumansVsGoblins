49ef7072b8568bfabd6b6e4c772810904fa83634
JRan-37 <79283020+JRan-37@users.noreply.github.com>
2022-05-02 11:08:58 -0400

    Updated entity spawning to reflect new constructors GameEvent listener for quit game input

--- src/java/Managers/GameManager.java
+++ src/java/Managers/GameManager.java
@@ -1,17 +1,18 @@
 package Managers;
 
+import Entities.Goblin;
 import Entities.Human;
 import Events.GameEvent;
 import Listeners.EventListener;
+import Utils.Events;
 import Utils.Position;
 
-public class GameManager{
+public class GameManager implements EventListener<GameEvent>{
 
     private static EventManager eventManager;
     private static GridManager gridManager;
     private static InputManager inputManager;
     private static GameManager instance = new GameManager();
-    public int x = 5;
 
     public static GameManager getInstance() {
         return instance;
@@ -23,15 +24,19 @@ public class GameManager{
         eventManager = EventManager.getInstance();
         gridManager = GridManager.getInstance();
         inputManager = InputManager.getInstance();
+
+        EventListener.EventStartListening(this, Events.GameEvent);
     }
     public void run() {
-        x = 12;
+
         //display title
         //display intro text
         //create game level
-        Human player = new Human(10, 10);
+        Human player = new Human(25, 8, 3 , 85);
         player.spawn(gridManager.getMap().getTileAtPosition(new Position(0, 0)));
-        player.move(gridManager.getMap().getTileAtPosition(new Position(1, 1)));
+
+        Goblin enemy1 = new Goblin(10, 5, 1, 60);
+        enemy1.spawn(gridManager.getMap().getTileAtPosition(new Position(3, 3)));
         //player.spawn(gridManager.getMap().getTileAtPosition(new Position(2, 2)));
 
         running = true;
@@ -42,15 +47,13 @@ public class GameManager{
             inputManager.getInput();
         }
 
-        player.spawn(gridManager.getMap().getTileAtPosition(new Position(3, 3)));
-        gridManager.displayMap();
 
     }
 
-    /*@Override
+    @Override
     public void onEvent(GameEvent eventType) {
         if(eventType.name.equals("End Game")){
             running = false;
         }
-    }*/
+    }
 }
